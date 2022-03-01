4d5ceeca8aef0bd6aab04d6e01cde9a262e032ed
Venkat Parsi <parsi.venkatramana@gmail.com>
2022-03-01 12:45:01 +0530

    new changes

--- notes-app/src/App/components/Notes/views/Note.js
+++ notes-app/src/App/components/Notes/views/Note.js
@@ -6,21 +6,21 @@ const Note = () => {
     ss = "00";
   return (
     <>
-      <div href="#0" class="button-green note" >
-        <span class="section"></span>
-        <div class="row-lay" >
+      <div href="#0" className="button-green note" >
+        <span className="section"></span>
+        <div className="row-lay" >
           <CircularTextButton className="col-1"></CircularTextButton>
           <div class="column-lay title">
             <span>Section 1: Getting Started ehat this is really working welll in this scenario.</span>
             <span>⓵ exp: Mar 12, 2013 </span>
           </div>
-          <div class="price column-lay timer shadow ">
-            <div class="blink-bg">
+          <div className="price column-lay timer shadow ">
+            <div className="blink-bg">
               {hh}:{mm}:{ss}
             </div>
-            <div class="row-lay flex-space-evenly">
+            <div className="row-lay flex-space-evenly">
               <i
-                class="bi-alarm "
+                className="bi-alarm "
                 style={{ fontSize: "large", fontWeight: "bold" }}
               ></i>
               <span>30m</span>
--- notes-app/src/App/components/VideoContainer/VideoContainer.js
+++ notes-app/src/App/components/VideoContainer/VideoContainer.js
@@ -3,11 +3,11 @@ import "./VideoContainer.css";
 const VideoContainer = () => {
   
   return (    
-        <div class="video-container">
+        <div className="video-container">
           <iframe 
             src="https://www.youtube.com/embed/9VNTvwaJ6oM"
-            frameborder="0"
-            allowfullscreen
+            frameBorder="0"
+            allowFullScreen
           ></iframe>
         </div>
   );
